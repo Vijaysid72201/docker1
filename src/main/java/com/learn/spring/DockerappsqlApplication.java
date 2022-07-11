f0408903625b41588a24d43f81ce44dab1a4fb4f
vijay jalgi <vijaykumarjalgi123@gmail.com>
2022-07-11 18:47:03 +0530

    docker1

---
+++ src/main/java/com/learn/spring/DockerappsqlApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerappsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerappsqlApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/learn/spring/model/Student.java
@@ -0,0 +1,13 @@
+package com.learn.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	private int id;
+	private String name;
+
+}
